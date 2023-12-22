d1488ae0732e48dc516be26de4ef1ce64ec681da
SettingDust <settingdust@gmail.com>
2023-12-22 20:36:45 +0800

    feat(fwaystones): support config screen

--- mod/src/client/kotlin/settingdust/betteruitextures/client/FabricWaystonesAssetsGenerator.kt
+++ mod/src/client/kotlin/settingdust/betteruitextures/client/FabricWaystonesAssetsGenerator.kt
@@ -32,6 +32,10 @@ object FabricWaystonesAssetsGenerator :
             Identifier(FabricWaystones.MOD_ID, "gui/waystone"),
             Waystone.generate(manager)
         )
+        dynamicPack.addAndCloseTexture(
+            Identifier(FabricWaystones.MOD_ID, "gui/waystone_config"),
+            Config.generate(manager)
+        )
     }
 
     private object Waystone {
@@ -67,6 +71,69 @@ object FabricWaystonesAssetsGenerator :
         fun generate(manager: ResourceManager): TextureImage {
             val original =
                 TextureImage.open(manager, Identifier(FabricWaystones.MOD_ID, "gui/waystone"))
+            val image = original.makeCopy()
+
+            // Background
+            for (x in 0 until WINDOW_WIDTH) {
+                for (y in 0 until WINDOW_HEIGHT) {
+                    if (x < GenericTextures.UNIT || x > WINDOW_WIDTH - GenericTextures.UNIT) {
+                        image.setFramePixel(0, x, y, 0)
+                    } else if (
+                        y < GenericTextures.UNIT || y > WINDOW_HEIGHT - GenericTextures.UNIT
+                    ) {
+                        image.setFramePixel(0, x, y, 0)
+                    } else {
+
+                        val originalColor = image.getFramePixel(0, x, y)
+                        if (originalColor == -0x39393A) image.setFramePixel(0, x, y, 0)
+                    }
+                }
+            }
+
+            val transformed = manager.let { generateWindowBackground(it) }
+
+            transformed.applyOverlay(image)
+
+            return transformed
+        }
+    }
+
+    private object Config {
+        private const val WINDOW_WIDTH = 177
+        private const val WINDOW_HEIGHT = 176
+
+        fun generateWindowBackground(manager: ResourceManager): TextureImage {
+            val background = GenericTextures.getGuiBackground(manager)
+
+            val scaledBackground =
+                background.resizeNinePatch(
+                    GenericTextures.UNIT to GenericTextures.UNIT,
+                    GenericTextures.UNIT to GenericTextures.UNIT,
+                    WINDOW_WIDTH to WINDOW_HEIGHT
+                )
+
+            val alignedBackground =
+                TextureImage.createNew(TEXTURE_SIZE, TEXTURE_SIZE, scaledBackground.metadata).also {
+                    ImageTransformer.builder(
+                            WINDOW_WIDTH,
+                            WINDOW_HEIGHT,
+                            TEXTURE_SIZE,
+                            TEXTURE_SIZE
+                        )
+                        .apply { copyRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0) }
+                        .build()
+                        .apply(scaledBackground, it)
+                }
+
+            return alignedBackground
+        }
+
+        fun generate(manager: ResourceManager): TextureImage {
+            val original =
+                TextureImage.open(
+                    manager,
+                    Identifier(FabricWaystones.MOD_ID, "gui/waystone_config")
+                )
             val image = original.makeCopy()
 
             // Background
